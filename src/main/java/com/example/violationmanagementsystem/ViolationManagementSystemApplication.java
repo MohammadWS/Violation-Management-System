33e0097cdfe075ac3f7616f50aae1fb6e76e12fc
MohammadWS <mwso2003@gmail.com>
2026-09-16 13:52:36 +0300

    feat: project setup

---
+++ src/main/java/com/example/violationmanagementsystem/ViolationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.violationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViolationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViolationManagementSystemApplication.class, args);
+	}
+
+}
